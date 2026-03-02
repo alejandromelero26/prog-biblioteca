f7b6093129edd55e5a8c23bb5c68f94284bee04c
Alejandro <amelevel@myuax.com>
2026-03-02 12:08:19 +0100

    feat(Biblioteca): creada la clase Biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,48 @@
 public class Biblioteca {
+
+    private Libro[] catalogo;
+    private final int CANT_MAX_LIBROS;
+
+    public Biblioteca(int cantidad_maxima_libros) {
+        CANT_MAX_LIBROS = cantidad_maxima_libros;
+        catalogo = new Libro[CANT_MAX_LIBROS];
+    }
+
+    /**
+     * Busca el primer libro que encuentre con el id recibido
+     * @param idLibro
+     * @return
+     */
+    public Libro findLibro(int idLibro){
+        boolean seguirBuscando = true;
+        Libro LibroEncontrado = null;
+
+        for(int i = 0; i < CANT_MAX_LIBROS && seguirBuscando; i++){
+            if(catalogo[i] != null && catalogo[i].getId() == idLibro){
+                seguirBuscando = false;
+                LibroEncontrado = catalogo[i];
+            }
+
+        }
+        return LibroEncontrado;
+    }
+
+    /**
+     * Metodo privado que busca la primera posicion libre en el catalogo
+     * @return -1 si no hay posiciion libre
+     */
+    private int buscarPrimerHuecolIBRE(){
+        int posicionLibre = -1;
+        boolean seguirBuscando = true;
+
+        for(int i = 0; i < CANT_MAX_LIBROS && seguirBuscando; i++){
+            if(catalogo[i] != null){
+                posicionLibre = i;
+                seguirBuscando = false;
+            }
+        }
+        return posicionLibre;
+    }
+
+
 }
