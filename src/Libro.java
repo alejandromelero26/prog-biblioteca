4eb0587a2a067c8a25821642bc52e2411618a1ca
alejandromelero26 <amelevel@myuax.com>
2026-03-03 00:04:03 +0100

    fix Libro

--- src/Libro.java
+++ src/Libro.java
@@ -1,18 +1,20 @@
 public class Libro {
+
+    // atributos de la clase
     private int id;
     private String titulo;
     private String autor;
     private int numPaginas;
 
     /**
-     * Metodo constructor de libros
-     * @param id
-     * @param titulo
-     * @param autor
-     * @param numPaginas
+     * Metodo constructor de objetos tipo {@code Libro}
+     * @param id identificador del libro
+     * @param titulo título del libro
+     * @param autor autor del libro
+     * @param numPaginas número de páginas del libro
      */
-    public Libro(int id, String titulo, String autor, int numPaginas) {
-        int numHojas;
+    public Libro(int id, String titulo, String autor, int numPaginas){  // parámetros del metodo
+        int numHojas;  // variable del metodo
 
         this.id = id;
         this.titulo = titulo;
@@ -24,41 +26,64 @@ public class Libro {
 
     /**
      * Devuelve el id del libro
-     * @return
+     * @return id del libro
      */
     public int getId() {
         return id;
     }
 
-    public String getTitulo() {
+    /**
+     * Devuelve el título del libro
+     * @return el título del libro
+     */
+    public String getTitulo(){
         return titulo;
     }
-    public String getAutor() {
+
+    /**
+     * Devuelve el autor del libro
+     * @return el autor del libro
+     */
+    public String getAutor(){
         return autor;
     }
-    public int getNumPaginas() {
+    /**
+     * Devuelve el número de páginas del libro
+     * @return el número de páginas del libro
+     */
+    public int getNumPaginas(){
         return numPaginas;
     }
 
+    /**
+     * Devuelve una representación textual del libro con todos sus atributos.
+     *
+     * @return cadena de texto formateada con la información del libro.
+     */
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("Libro: ");
+        StringBuilder sb = new StringBuilder("Libro: {");
 
         sb.append(String.format("\n id: %d;", id));
-        sb.append(String.format("\n titulo: %s;", titulo));
-        sb.append(String.format("\n autor: %s;", autor));
-        sb.append(String.format("\n numPaginas: %s;", numPaginas));
+        sb.append(String.format(" titulo: %s;", titulo));
+        sb.append(String.format(" autor: %s;", autor));
+        sb.append(String.format(" num. páginas: %d;", numPaginas));
         sb.append("}");
 
         return sb.toString();
     }
 
+    /**
+     * Actualiza el número de páginas que tiene el libro. Comprueba que el número recibido sea mayor que 0
+     * @param numPaginas nueva cantidad de páginas
+     * @return true si ha podido cambiarse, false si no ha podido cambiarse
+     */
     public boolean updateNumPaginas(int numPaginas) {
         boolean resultado;
 
-        if(numPaginas <= 0){
+        if( numPaginas <= 0 ){
             resultado = false;
-        } else{
+        } else {
             this.numPaginas = numPaginas;
             resultado = true;
         }
